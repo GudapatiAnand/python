32cf0723769d1801dc65a9fddc665a439f8d3dc8
GudapatiAnand <gudapatianand52@gmail.com>
2024-01-02 18:17:13 +0530

    first commit

--- Calendar.py
+++ Calendar.py
@@ -2,6 +2,9 @@ from calendar import *
 
 year=int(input("Enter the year:"))
 
-print(calendar(year, 2,1,8,4))
-
 print(calendar(year, 3,1,8,4))
+
+#3 = 3 characters for days(Mon, Tue, Wed, etc)
+#1 = 1 line (row) for each week
+#8 = 8 rows for each month
+#4 = 4 colouns for all months of the year

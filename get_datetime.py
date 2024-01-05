3f31f7566f0e4b48413dbf1573e7ec74e3ab679c
GudapatiAnand <gudapatianand52@gmail.com>
2024-01-05 22:06:02 +0530

    first commit

---
+++ get_datetime.py
@@ -0,0 +1,6 @@
+#Python: Get the current Time 
+
+
+import datetime
+
+print(datetime.datetime.now().time())
---
+++ search_string.py
@@ -0,0 +1,10 @@
+#check that a string contains only a certain set of characters.
+
+import re
+
+def is_allowed(string):
+    char=re.compile(r'[^a-zA-Z0-9]')
+    str=char.search(string)
+    return not bool(str)
+print(is_allowed("Abcd1234"))
+print(is_allowed("##^$*$((#"))

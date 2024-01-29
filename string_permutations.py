af92b34b8def4329c90cd1bb2a211450aa7ced4a
GudapatiAnand <gudapatianand52@gmail.com>
2024-01-29 22:36:39 +0530

    commit

--- string_permutations.py
+++
@@ -1,9 +0,0 @@
-string=str(input("Enter the string:"))
-def get_permutations(string,i=0):
-    if i==len(string):
-        print("".join(string))
-    for j in range(i,len(string)):
-        words=[c for c in string]
-        words[i],words[j]=words[j],words[i]
-        get_permutations(words,i+1)
-get_permutations(string)

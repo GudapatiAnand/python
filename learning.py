2a6574d767b3b03f8bac0cb8506f139262546b06
GudapatiAnand <gudapatianand52@gmail.com>
2024-07-02 11:31:07 +0530

    commit

--- learning.py
+++ learning.py
@@ -1,3 +1,7 @@
-demo_list=[5,4,4,6,8,12,12,1,5]
-unique_list=list(set(demo_list))
-print(unique_list)
+# demo_list=[5,4,4,6,8,12,12,1,5]
+# unique_list=list(set(demo_list))
+# print(unique_list)
+
+import numpy as np
+arr=np.array([1,2,3,4,5])
+print(arr.argsort()[ -5: ][::-1])

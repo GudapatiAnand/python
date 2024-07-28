fb7479f51a915776073eacc120cb287a7bd836d7
GudapatiAnand <gudapatianand52@gmail.com>
2024-07-28 08:07:44 +0530

    commit

--- get_datetime.py
+++ get_datetime.py
@@ -1,5 +1,13 @@
-#Python: Get the current Time 
+#Python: Get the current Time method 1
 
-import datetime
+# import datetime
 
-print(datetime.datetime.now().time())
+# print(datetime.datetime.now().time())
+
+#method 2
+
+from datetime import datetime
+
+current_time=datetime.now().strftime("%H:%M:%S")
+
+print("Current Time:",current_time)

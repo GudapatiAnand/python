96a585a84aea2a7e074cca218c5432d3381dc9e8
GudapatiAnand <gudapatianand52@gmail.com>
2024-08-10 19:47:29 +0530

    commit

--- set_topic.py
+++ set_topic.py
@@ -1,28 +1,34 @@
 #sets and set methods
-#anand={1,1,2,3,3,4,5,4,829,575,229,348,19}
-#print(type(anand))
-#print(anand)
-#anand.add(235)
-#anand.update({1,88,22,66,78,5,4,"python"})
-#anand.remove(229)
-#anand.pop()
-#anand.clear()
-#b=anand.copy()
-#print(b)
+# anand={1,1,2,3,3,4,5,4,829,575,229,348,19}
+# print(type(anand))
+# print(anand)
+# anand.add(235)
+# print(anand)
+# anand.update({1,88,22,66,78,5,4,"python"})
+# print(anand)
+# anand.remove(229)
+# print(anand)
+# anand.pop()
+# print(anand)
+# anand.clear()
+# print(anand)
+# b=anand.copy()
+# print(b)
 
 
 #set operations
-#set1={1,2,3,4,5}
-#set2={4,5,6,7,8}
-#print(set1.union(set2))
-#print(set1.intersection(set2))
-#print(set1.difference(set2))
-#print(set1.symmetric_difference(set2))
-#set1={1,2,3}
-#set2={1,2,3,4,5}
-#print(set1.isdisjoint(set2))
-#print(set1.issubset(set2))
-#print(set2.issuperset(set1))
+# set1={1,2,3,4,5}
+# set2={4,5,6,7,8}
+# print(set1.union(set2)) # it will print all the elements in both the sets
+# print(set1.intersection(set2)) # it will print the common elements from both the sets
+# print(set1.difference(set2)) # it will print only set1 elements which is not common in both the sets.
+# print(set2.difference(set1)) # it will print only set2 elements which is not common in both the sets.
+# print(set1.symmetric_difference(set2)) # it will remove common elements and print remaining elements from both the sets.
+set1={1,2,3}
+set2={1,2,3,4,5}
+# print(set1.isdisjoint(set2)) # the elements in both the sets should be different then it will print true
+# print(set1.issubset(set2)) #it will print true if all the elements in set1 should present in set2 and it can be more elements in set2
+print(set2.issuperset(set1)) #it will print true if all the elements in set1 present in set2
 
 
 # for i in {1,2,3,4,5,6}:
@@ -34,7 +40,7 @@
 
 
 #frozenset
-j=[1,2,3,4,5]
-print(type(j))
-d=frozenset(j)
-print(list(d))
+# j=[1,2,3,4,5]
+# print(type(j))
+# d=frozenset(j)
+# print(list(d))
